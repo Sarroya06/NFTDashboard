3864c55053e77631d7d5c6dd13783da57459b7a9
Sarroya06 <Sarroya06@gmail.com>
2022-07-29 13:11:25 +0530

    angular material files deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatListModule} from '@angular/material/list';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -22,19 +13,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HeaderComponent,
     SidenavComponent,
     DashboardComponent
-
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatMenuModule,
-    MatDividerModule,
-    MatIconModule,
-    MatListModule,
-    MatSlideToggleModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
